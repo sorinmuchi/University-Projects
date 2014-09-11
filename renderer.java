61479d2abc07e79c7cf2beab0be116ce72491dfc
Gh0o0st <boggim@gmail.com>
2014-09-10 20:02:12 -0400

    Create renderer.java
    Renderer box

---
+++ renderer.java
@@ -0,0 +1,31 @@
+import com.sun.j3d.utils.geometry.Box;
+import java.util.HashMap;
+import javax.media.j3d.Appearance;
+import javax.vecmath.Point3d;
+/**
+ * @author Gh00st
+ */
+public class Renderer {
+    HashMap<String,Float> coordinates;
+    Box box;
+    Point3d center;
+    Bullet bullet;
+    Renderer(Bullet bullet)
+    {
+     this.bullet=bullet;
+     this.coordinates = new HashMap<>();
+     this.center=new Point3d (bullet.center);
+     coordinates.put("x",0.5f);
+     coordinates.put("y",0.5f);
+     coordinates.put("z",0.5f);
+     box=new Box(coordinates.get("x"),coordinates.get("y"),coordinates.get("z"),new Appearance());
+    }
+    public double getVolume()
+    {
+        return box.getZdimension()*box.getYdimension()*box.getXdimension();
+    }
+    public void move()
+    {
+        center=bullet.center;
+    }
+}
